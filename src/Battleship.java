f654e6060eb2c1cc11d07c0b7f7fd65db23b9f9d
andrewblodgett <61248022+andrewblodgett@users.noreply.github.com>
2020-12-30 11:07:05 -0700

    Create Battleship.java

---
+++ src/Battleship.java
@@ -0,0 +1,14 @@
+public class Battleship {
+  
+  public void playTurn(int turn) {
+  
+  }
+  
+  public boolean checkWin() {
+    return false;
+  }
+  
+  public void main(String[] args) {
+  
+  }
+}
