8c5c8fa464dade6ec56fe89007fbe36fd167271a
andrewblodgett <61248022+andrewblodgett@users.noreply.github.com>
2020-12-30 11:11:38 -0700

    Create GameBoard.java

---
+++ src/GameBoard.java
@@ -0,0 +1,20 @@
+public class GameBoard {
+  boolean[][] ships;
+  boolean[][] misses;
+  
+  public void reset() {
+  
+  }
+  
+  public void placeShip() {
+  
+  }
+  
+  public void removeShip() {
+  
+  }
+  
+  public boolean[][] getBoard() {
+    return ships;
+  }
+}
