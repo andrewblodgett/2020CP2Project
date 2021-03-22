1ecaec9c67f1292cc77b480f531b8746d6b8c62f
andrewblodgett <61248022+andrewblodgett@users.noreply.github.com>
2021-03-22 12:02:09 -0600

    Update BattleShip.java

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -1,38 +1,32 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class BattleShip {
-    public static void main(String[] args) {
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Battleship {
+    public static void main(String[] args) throws IOException {
         boolean play = true;
         Random rand = new Random();
         Board yourBoard = new Board();
         Board oppBoard = new Board();
         String validLetters = "ABCDEFGHIJ";
         Scanner in = new Scanner(System.in);
+        int numOfMoves = 0;
         while (play) {
 
             System.out.println("This is your board:");
             System.out.println(yourBoard.formatBoard());
-            // for(char[] a : yourBoard.getBoard()) {
-            //     for(char c: a) {
-            //         System.out.print(c + " ");
-            //     }
-            //     System.out.println("");
-            // }
+
             System.out.println("This is your opponent's board:");
             System.out.println(oppBoard.formatBoard());
-            // for(char[] a : oppBoard.getBoard()) {
-            //     for(char c: a) {
-            //         System.out.print(c + " ");
-            //     }
-            //     System.out.println("");
-            // }
 
             String yc = "";
             int xc = -1;
             while (!(validLetters.contains(yc) && (xc <= 10 && xc > 0))) {
                 System.out.print("Enter a letter and a number in the form 'A1': ");
-                String input = in.next().toUpperCase().strip();
+                String input = in.next().toUpperCase().trim();
                 if (input.length() > 1) {
                     if (input.length() > 3)
                         input = input.substring(0, 3);
@@ -48,16 +42,29 @@ public class BattleShip {
                         xc = -1;
                     }
                 }
-                
+
             }
 
             oppBoard.shoot(xc - 1, yc.charAt(0));
             yourBoard.shoot(rand.nextInt(10), validLetters.substring(rand.nextInt(10)).charAt(0));
-
-            if (oppBoard.checkWin())
+            numOfMoves++;
+            if (oppBoard.checkIfSunk().length() != 0) {
+              System.out.println("You sank my " + oppBoard.checkIfSunk());
+            }
+            if (oppBoard.checkWin()) {
                 play = false;
+                System.out.println(oppBoard.formatBoard() + "You Win!\n");
+                win(numOfMoves);
+            }
         }
 
         in.close();
+    }
+
+    public static void win(int score) throws IOException {
+        FileWriter fileWriter = new FileWriter("highscores.txt", true);
+        PrintWriter writer = new PrintWriter(fileWriter);
+        writer.print(score);
+        writer.close();
     }
 }
