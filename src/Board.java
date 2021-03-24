218e105d02fbe0c2897824e7f06b9d556e185a46
andrewblodgett <61248022+andrewblodgett@users.noreply.github.com>
2021-03-24 13:27:05 -0600

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,36 +1,72 @@
+/**
+ * The Board 
+ * This class represents the board for battleship. 
+ * @author Andrew Blodgett
+ * @since 2021-3-24
+ * @version 1.0
+ */
+
 import java.util.Random;
 
 public class Board {
     private char[][] board = generateBoard();
-    boolean sunkCarrier = false;
-    boolean sunkBattleship = false;
-    boolean sunkDestroyer = false;
-    boolean sunkSubmarine = false;
-    boolean sunkPatrolBoat = false;
 
-
-    public Board() {}
-
-
+    /**
+     * Returns the board.
+     * @return 
+     */
     public char[][] getBoard() {
         return board;
     }
 
+    /**
+     * Returns a version of the board where you can't see the ships.
+     * @return
+     */
+    public char[][] getHiddenBoard() {
+        char[][] hiddenBoard = new char[10][10];
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                if (board[i][j] == 'o' || board[i][j] == 'x') {
+                    hiddenBoard[i][j] = board[i][j];
+                } else {
+                    hiddenBoard[i][j] = '.';
+                }
+            }
+        }
+        return hiddenBoard;
+    }
+
+    /**
+     * Checks to see if a square on this board has already been shot at.
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @return true if that space has been shot at
+     */
     public boolean checkIfAlreadyShot(int x, int y) {
         if (board[y][x] == 'o' || board[y][x] == 'x') return true;
         else return false;
     }
 
+    /**
+     * Mutator method that 'shoots' at a square on the board, turning it to a hit if it was a ship, or a miss if it was water.
+     * @param x the x coordinate
+     * @param yLetter the letter for the y coordinate
+     */
     public void shoot(int x, char yLetter) {
         int y = "ABCDEFGHIJ".indexOf(yLetter);
 
         if ("12345".indexOf(board[y][x]) != -1) {
             board[y][x] = 'x';
-        } else if (board[y][x] == 'W'){
+        } else if (board[y][x] == '.'){
             board[y][x] = 'o';
         }
     }
 
+    /**
+     * Checks if this board has no ships unsunk.
+     * @return whether or not you have won
+     */
     public boolean checkWin() {
         boolean win = true;
         loop:
@@ -45,7 +81,11 @@ public class Board {
         return win;
     }
 
-    public String formatBoard() {
+    /**
+     * This method gets a formatted version of the board that doesn't show the locations of the ships.
+     * @return a string representing the board
+     */
+    public String formatHiddenBoard() {
         String formatted = "  ";
         for (int i = 0; i < board.length; i++) {
             formatted = formatted +  (i + 1) + " ";
@@ -64,51 +104,53 @@ public class Board {
         return formatted;
     }
 
-    public String checkIfSunk() {
-        sunkCarrier = true;
-        sunkBattleship = true;
-        sunkDestroyer = true;
-        sunkSubmarine = true;
-        sunkPatrolBoat = true;
+    /**
+     * This method gets a formatted version of the board that shows the locations of the ships.
+     * @return a string representing the board
+     */
+    public String formatBoard() {
+        String formatted = "  ";
+        for (int i = 0; i < board.length; i++) {
+            formatted = formatted +  (i + 1) + " ";
+        }
+        for (int i = 0; i < board.length; i++) {
+            char letter = "ABCDEFGHIJ".charAt(i);
+            formatted = formatted + "\n" + letter + " ";
+            for (int j = 0; j < board[0].length; j++) {
+                formatted = formatted + board[i][j] + " ";
+            }
+        }
+        return formatted;
+    }
+
+    /**
+     * Checks if a certain ship is sunk.
+     * @param shipNum the number of a ship (5 for carries, 4 for battleship, 3 for destroyer, 2 for patrol boat, 1 for submarine)
+     * @return true if there are noe spaces of that ship left on the board.
+     */
+    public boolean checkIfSunk(int shipNum) {
+        boolean sunk = true;
         for (char[] row : board) {
             for (char space : row) {
-                switch(space) {
-                    case '1':
-                        sunkSubmarine = false;
-                        break;
-                    case '2':
-                        sunkPatrolBoat = false;
-                        break;
-                    case '3':
-                        sunkDestroyer = false;
-                        break;
-                    case '4':
-                        sunkBattleship = false;
-                        break;
-                    case '5':
-                        sunkCarrier = false;
-                        break;
-                    default:
-                        break;
+                if (space == Character.forDigit(shipNum, 10)) {
+                    sunk = false;
                 }
             }
         }
-        if (sunkCarrier)  return "Carrier";
-        if (sunkBattleship)  return "Battleship";
-        if (sunkDestroyer)  return "Destroyer";
-        if (sunkSubmarine)  return "Submarine";
-        if (sunkPatrolBoat)  return "Patrol Boat";
-        return "";
+        return sunk;
     }
 
-
+    /**
+     * This method randomly generates each board, making sure no ships overlap.
+     * @return a randomly generated 2D char array
+     */
     private char[][] generateBoard() {
         Random rand = new Random();
         char[][] newBoard = new char[10][10];
 
         for (int i = 0; i < newBoard.length; i++) {
             for (int j = 0; j < newBoard[0].length; j++) {
-                newBoard[i][j] = 'W';
+                newBoard[i][j] = '.';
             }
         }
         for (int j = 5; j > 0; j--) {
@@ -154,6 +196,4 @@ public class Board {
         }
         return newBoard;
     }
-
-
 }
